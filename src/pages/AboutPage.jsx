b2677096dc8d877b8ed3b1a965da9ed873ff1816
Syed Rabiaan <syedrabiaan@gmail.com>
2026-07-02 16:49:07 +0500

    Update AboutPage styles and component

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { ArrowRight, Star, CheckCircle, Clock, DollarSign, MessageCircle, Shield, Eye } from 'lucide-react';
 import Footer from '../components/Footer';
 import './AboutPage.css';
